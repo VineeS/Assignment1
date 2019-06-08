21f6b7ba0b81e5e7ee20c377ef80a032d23ae268
VineeS <shuklavinee@gmail.com>
2019-06-08 17:03:05 -0400

    6/7/2019 Commit

---
+++ Assignment1/String_Understanding.py
@@ -0,0 +1,25 @@
+str1 = "Hello"
+str2 = "World"
+temp = str1 + str2
+print(temp)
+
+length = len(str1)
+print(length)
+str3 = "123"
+length = len(str3)
+print(length)
+temp1 = int(str3)+1
+# here str3 + 1 will give the error
+print(temp1)
+
+fruit = "Apple"
+x = fruit[0]
+print(x)
+
+
+n = 3
+y = fruit[n-1]
+print(y)
+# 0 - A , 1 - p , 2 - p
+
+
